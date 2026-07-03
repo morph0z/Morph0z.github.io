ccc7334503b7e1346f9807c52845ab5472923835
Morph0z <jamal009a@gmail.com>
2026-07-03 15:29:13 -0400

    slide transitions, and new renders, and new music

--- musicPlayer.js
+++ musicPlayer.js
@@ -27,7 +27,9 @@ const songs = ['practice24','practice32','practice43','practice44','practice50'
                      ,'practice251full','practice252','practice255','practice258','practice260','practice264','practice268','practice269',
                      'practice269fuller','practice271','practice272','practice273','practice274','practice274full','practice277fuller','practice284',
                       'practice286','practice289slightlybitmoreBetter','practice299','practice300','practice301','practice303','practice304',
-                      'practice305','practice309','practice309pianoSolo','practice311','practice314','practice316'
+                      'practice305','practice309','practice309pianoSolo','practice311','practice314','practice316','practice317','practice317bit'
+                      ,'practice318','practice319pre','practice319','practice320','practice322','practice324','practice327','practice328','practice330',
+                      'practice331'
               ];
 
 // Keep track of song
