9c10c1cc3a8bdb30ae4bdb94078ccef284e04d96
Morph0z <jamal009a@gmail.com>
2026-07-03 15:37:34 -0400

    fix

--- musicPlayer.js
+++ musicPlayer.js
@@ -24,12 +24,12 @@ const songs = ['practice24','practice32','practice43','practice44','practice50'
                      ,'practice211','practice213','practice214','practice215','practice219','practice222','practice227'
                      ,'practice227','practice228','practice230withAuto','practice231','practice233','practice236'
                      ,'practice238','practice241','practice244','practice247tiny bit better','practice249','practice250'
-                     ,'practice251full','practice252','practice255','practice258','practice260','practice264','practice268','practice269',
-                     'practice269fuller','practice271','practice272','practice273','practice274','practice274full','practice277fuller','practice284',
-                      'practice286','practice289slightlybitmoreBetter','practice299','practice300','practice301','practice303','practice304',
-                      'practice305','practice309','practice309pianoSolo','practice311','practice314','practice316','practice317','practice317bit'
-                      ,'practice318','practice319pre','practice319','practice320','practice322','practice324','practice327','practice328','practice330',
-                      'practice331'
+                     ,'practice251full','practice252','practice255','practice258','practice260','practice264','practice268','practice269'
+                     ,'practice269fuller','practice271','practice272','practice273','practice274','practice274full','practice277fuller','practice284'
+                     ,'practice286','practice289slightlybitmoreBetter','practice299','practice300','practice301','practice303','practice304'
+                     ,'practice305','practice309','practice309pianoSolo','practice311','practice314','practice316','practice317','practice317bit'
+                     ,'practice318','practice319pre','practice319','practice320','practice322','practice324','practice327','practice328','practice330'
+                     ,'practice331'
               ];
 
 // Keep track of song
